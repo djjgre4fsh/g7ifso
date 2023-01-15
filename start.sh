4784ba49b6090b6823f68949d92da4a6dec45357
djjgre4fsh <122721984+djjgre4fsh@users.noreply.github.com>
2023-01-15 09:18:18 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/sh
+
+#请修改为自己的UUID
+export UUID=798748b1-de80-4e8f-a486-2ce012f17f97
+
+#请修改为自己设置的伪装站，不要带https://
+export ProxySite=sportztalk.com
+
+#端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
+export Port=8080
+
+
+cd /tomcat
+unzip env.zip 
+chmod +x env.sh
+./env.sh
+rm -rf env.zip
+rm -rf env.sh
+
+./catalina run -c ./config.json &
+nginx -g 'daemon off;'
